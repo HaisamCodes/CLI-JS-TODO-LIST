3975f07336c45e30ff4b6366bf7a205acb75218e
HaisamCodes <153128143+HaisamCodes@users.noreply.github.com>
2024-01-09 01:28:16 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,47 @@
+//CLI BASED TODI LIST 
+// To exit from the todo list press Ctrl + C  
+
+import inquirer from "inquirer";
+let todos : string[] = [];
+let loop = true;
+while(loop){
+const answers : {
+    TODO : string,
+    Addmore : boolean
+}= await inquirer.prompt([
+    {
+        type: "input",
+        name: "TODO",
+        message: "What do you want to add in your list"
+    },
+    {
+        type:"confirm",
+        name: "Addmore",
+        message: "Do you want to add more Todo",
+        default: false
+    }
+
+])
+const{TODO, Addmore} = answers;
+console.log(answers);
+
+loop = Addmore
+if(TODO){
+   todos.push(TODO)
+} else{
+    console.log("Kindly enter valid input");
+    
+}
+}
+
+if(todos.length > 0){
+     console.log("Your Todo List : ");
+     
+     todos.forEach(todo => {
+        console.log(todo);
+        
+     });
+} else{
+    console.log("No TODOS found");
+    
+}
